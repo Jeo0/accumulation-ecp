cede8ad938626c6ea9cf3106563a4a5beb50b215
Jeo0 <jjrelb@gmail.com>
2025-03-27 20:09:21 +0800

    removed unneccessary methods

--- Plesson3_printform.cs
+++ Plesson3_printform.cs
@@ -12,26 +12,19 @@ namespace lesson3___example3
 {
     public partial class Plesson3_printform: Form
     {
-        public Plesson3_printform()
-        {
+        public Plesson3_printform() {
             InitializeComponent();
             //codes below is to display all the contents of the listbox
             //from the Activity4.cs form to this current form
             printDisplayListBox.Items.AddRange(printDisplayListBox.Items);
 
         }
-        public ListBox PrintDisplayListBox
-        {
+        public ListBox PrintDisplayListBox {
             get { return printDisplayListBox; }
         }
 
-        private void printDisplayListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
 
-        }
-
-        private void Activity4_PrintFrms_Load(object sender, EventArgs e)
-        {
+        private void Activity4_PrintFrms_Load(object sender, EventArgs e) {
 
         }
     }
