60960bb6b949daf61209bc2a41b540081b336d40
Jeo0 <jjrelb@gmail.com>
2025-03-27 23:31:02 +0800

    added classes

--- test.cs
+++ test.cs
@@ -4,5 +4,16 @@ public class Class1
 {
 	public Class1()
 	{
+    
+        private string picpath;
+        private Double basic_netincome = 0.00,
+            basic_numhrs = 0.00,
+            basic_rate = 0.00,
+            hono_netincome = 0.00,
+            hono_numhrs = 0.00,
+            hono_rate = 0.00,
+            other_netincome = 0.00,
+            other_numhrs = 0.00,
+            other_rate = 0.00;
 	}
 }
