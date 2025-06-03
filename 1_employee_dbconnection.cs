b56a598f9c8b89d088e3fb9b88567d152e4115a0
ChristianJudeEraldo <christian.eraldo@lpunetwork.edu.ph>
2025-06-03 08:17:32 +0800

    acts 1-10

--- 1_employee_dbconnection.cs
+++ 1_employee_dbconnection.cs
@@ -36,43 +36,59 @@ namespace projectNamin
 
         public void employee_sqladapterSelect() // Executes SELECT query
         {
-            employee_sql_connection.Open(); // Open connection before executing
+          
             employee_sql_dataadapter = new SqlDataAdapter();
             employee_sql_dataadapter.SelectCommand = employee_sql_command;
+            if (employee_sql_connection.State == ConnectionState.Closed)
+            {
+                employee_sql_connection.Open();
+            }
             employee_sql_command.ExecuteNonQuery(); // Not always needed for SELECT, but keeping your format
-            employee_sql_connection.Close(); // Close connection after executing
+           
         }
 
         public void employee_sqladapterInsert() // Executes INSERT query
         {
-            employee_sql_connection.Open();
+        
             employee_sql_dataadapter = new SqlDataAdapter();
             employee_sql_dataadapter.InsertCommand = employee_sql_command;
+            if (employee_sql_connection.State == ConnectionState.Closed)
+            {
+                employee_sql_connection.Open();
+            }
             employee_sql_command.ExecuteNonQuery();
-            employee_sql_connection.Close();
+       
         }
 
         public void employee_sqladapterDelete() // Executes DELETE query
         {
-            employee_sql_connection.Open();
+           
             employee_sql_dataadapter = new SqlDataAdapter();
             employee_sql_dataadapter.DeleteCommand = employee_sql_command;
+            if (employee_sql_connection.State == ConnectionState.Closed)
+            {
+                employee_sql_connection.Open();
+            }
             employee_sql_command.ExecuteNonQuery();
-            employee_sql_connection.Close();
+          
         }
 
         public void employee_sqladapterUpdate() // Executes UPDATE query
         {
-            employee_sql_connection.Open();
+        
             employee_sql_dataadapter = new SqlDataAdapter();
             employee_sql_dataadapter.UpdateCommand = employee_sql_command;
+            if (employee_sql_connection.State == ConnectionState.Closed)
+            {
+                employee_sql_connection.Open();
+            }
             employee_sql_command.ExecuteNonQuery();
-            employee_sql_connection.Close();
+
         }
 
         public void employee_sqldatasetSELECT() // Fills dataset with SELECT results
         {
-            employee_sql_connection.Open();
+           
             employee_sql_dataset = new DataSet();
             employee_sql_dataadapter.Fill(employee_sql_dataset, "pos_empRegTbl");
             employee_sql_connection.Close();
